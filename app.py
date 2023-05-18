280ab275decff373ded88f79a0c72954d36b00c4
rijfas <rijfas01@gmail.com>
2023-05-18 17:10:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, Response
+from fer import FER
+from fer.utils import draw_annotations
+import cv2
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+
+def detect_emotion(frame):
+    detector = FER(mtcnn=True)
+    emotions = detector.detect_emotions(frame)
+    return draw_annotations(frame, emotions)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', detect_emotion(frame))
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+            
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
